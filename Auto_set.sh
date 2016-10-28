50927b7b800c0b835a52d6fd969341a95fca1284
Aiclear <446798939@qq.com>
2016-10-29 01:35:15 +0800

    modify a little

--- Auto_set.sh
+++ Auto_set.sh
@@ -27,16 +27,18 @@ read -p "Install jdk [y|n]" ans
 
 if [ "$ans"x = "y"x ]; then
 #配置安装jdk
+    read -p "Please input your jdk file path: " path
     cd /lib
     mkdir java-sun
-    tar xf /home/xinzhe/Downloads/jdk-* -C /lib/java-sun/
+    tar xf $path/jdk-* -C /lib/java-sun/
 
     cd /etc/profile.d/
     touch java.sh
     echo "export JAVA_HOME=/lib/java-sun/$(ls /lib/java-sun/ | grep jdk-*)" >> /etc/profile.d/java.sh
     echo "export PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile.d/java.sh
     echo "export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar" >> /etc/profile.d/java.sh
-else
+    source /etc/profile
+  else
     echo "OK"
     exit 0
 fi
