95254acaa0a2d5405ad43e925b22f4ff635dfe31
Aiclear <446798939@qq.com>
2017-09-14 11:13:00 +0800

    rewrite port of code that about install command mkid

--- auto_set.sh
+++ auto_set.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 #配置为我自己的.vimrc文件
 setVim() {
     cd $HOME
@@ -40,18 +38,32 @@ which mkid 1> /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "mkid command is existed."
 else
-    mkid_file=`ls /tmp | grep 'idutils-*'`
-    if [ ! -f $mkid_file ]; then
+    mkid_file=$(ls /tmp/idutils-* 2> /dev/null)
+    if [ ! -n "$mkid_file" ]; then
+        echo "==> Downloading idutils packages......"
         cd /tmp
         wget http://ftp.gnu.org/gnu/idutils/idutils-4.6.tar.xz
         tar -xf idutils-4.6.tar.xz
     fi
 
-
     # install mkid command
-    cd idutils-4.6
-    sed -i "1030s#^\(_GL_WARN_ON_USE\)#//\1#g" lib/stdio.h
+    echo "==> Install idutils packages......"
+    if [ -f /tmp/idutils-4.6.tar.xz ]; then
+        cd /tmp
+        tar -xf idutils-4.6.tar.xz
+    fi
+    cd /tmp/idutils-4.6
+    ./configure 1> /dev/null 2>&1
+    make        1> /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        cd /tmp/idutils-4.6/lib/
+        # 修改文件，不然会编译错误
+        sed -i "1030s#^\(_GL_WARN_ON_USE\)#//\1#g" stdio.h
+        cd /tmp/idutils-4.6/
+    fi
 
-    ./config && make && make install
+    sudo make && sudo make install
+    if [ $? -eq 0 ]; then
+        echo "==> Install idutils success......"
+    fi
 fi
-
